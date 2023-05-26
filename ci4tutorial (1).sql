4ae2df37190b85f6132a5d0133d0ff1723c6fbc8
NatalyMolano <104031761+NatalyMolano@users.noreply.github.com>
2023-05-26 00:29:16 -0500

    Add files via upload

---
+++ ci4tutorial (1).sql
@@ -0,0 +1,294 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-05-2023 a las 07:28:36
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ci4tutorial`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `agendamiento`
+--
+
+CREATE TABLE `agendamiento` (
+  `id` int(11) NOT NULL,
+  `documento` int(11) NOT NULL,
+  `especialidad` varchar(20) NOT NULL,
+  `documento_doctor` int(11) NOT NULL,
+  `Fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `doc_documento` int(11) NOT NULL,
+  `Nombre` varchar(20) NOT NULL,
+  `Especialidad` varchar(20) NOT NULL,
+  `Fecha` date NOT NULL,
+  `cantidad_citas` int(11) NOT NULL,
+  `slug` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `doctor`
+--
+
+INSERT INTO `doctor` (`doc_documento`, `Nombre`, `Especialidad`, `Fecha`, `cantidad_citas`, `slug`) VALUES
+(0, 'camila', 'Medicina', '2023-05-01', 10, 'camila'),
+(367234687, 'carlos', 'Medicina', '2023-05-01', 10, 'carlos'),
+(2147483647, 'camila', 'Medicina', '2023-05-01', 10, 'camila');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `especialidad`
+--
+
+CREATE TABLE `especialidad` (
+  `id` int(11) NOT NULL,
+  `Especialidad médica` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `especialidad`
+--
+
+INSERT INTO `especialidad` (`id`, `Especialidad médica`) VALUES
+(1, 'Medicina General'),
+(2, 'pediatría');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `frutas`
+--
+
+CREATE TABLE `frutas` (
+  `id_fruta` int(11) NOT NULL,
+  `Nombre` varchar(20) NOT NULL,
+  `Precio` int(11) NOT NULL,
+  `Cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mascotas`
+--
+
+CREATE TABLE `mascotas` (
+  `id_com` int(11) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `comentaria` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `news`
+--
+
+CREATE TABLE `news` (
+  `title` varchar(128) NOT NULL,
+  `slug` varchar(128) NOT NULL,
+  `body` text NOT NULL,
+  `Fecha_cita` date NOT NULL,
+  `cupos` int(11) NOT NULL,
+  `documento` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `news`
+--
+
+INSERT INTO `news` (`title`, `slug`, `body`, `Fecha_cita`, `cupos`, `documento`) VALUES
+('camila', 'camila', 'Odontologia', '2023-05-25', 3, 327323826),
+('nataly', 'nataly', 'ortodoncia', '2023-05-11', 12, 362736261);
+
+--
+-- Disparadores `news`
+--
+DELIMITER $$
+CREATE TRIGGER `update_cupos` AFTER UPDATE ON `news` FOR EACH ROW BEGIN
+    IF NEW.Fecha_cita < CURDATE() THEN
+        UPDATE news SET cupos = 0 WHERE documento = NEW.documento;
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paciente`
+--
+
+CREATE TABLE `paciente` (
+  `documento` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `sexo` varchar(20) NOT NULL,
+  `EPS` varchar(20) NOT NULL,
+  `fecha_de_nacimiento` date NOT NULL,
+  `email` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paises`
+--
+
+CREATE TABLE `paises` (
+  `codigo` varchar(3) NOT NULL,
+  `pais` varchar(20) NOT NULL,
+  `ciudad` varchar(20) NOT NULL,
+  `continente` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `documento` int(11) NOT NULL,
+  `sexo` varchar(20) NOT NULL,
+  `EPS` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `username`, `email`, `password`, `created_at`, `updated_at`, `documento`, `sexo`, `EPS`) VALUES
+(9, 'Juanes', 'Juan@gmail.com', '$2y$10$VU3Reca9K85mHvz5lcNW0egmDMRXlOA1QQyt3Ge52zux91KVQ0x2y', '2023-05-25 19:17:31', '2023-05-25 19:17:31', 2147483647, 'femenino', 'compensar'),
+(10, 'Camilo Perez', 'camilo@gmail.com', '$2y$10$iPLYbI2A50g8o6daGXBs5.sXpyYBAeDcCRYxxQSi/VSsFcyaSNYBW', '2023-05-25 19:59:22', '2023-05-25 19:59:22', 437487839, 'masculino', 'compensar'),
+(13, 'maria', 'maria@gmail.com', '$2y$10$Vl43XtL6b16BNym0OJxlLOT8gxlDbdXKJMe/bgyrFgtyuHCPJsLf.', '2023-05-25 20:00:25', '2023-05-25 20:00:25', 24734792, 'femenino', 'compensar');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `agendamiento`
+--
+ALTER TABLE `agendamiento`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `documento` (`documento`,`documento_doctor`),
+  ADD KEY `documento_doctor` (`documento_doctor`);
+
+--
+-- Indices de la tabla `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`doc_documento`);
+
+--
+-- Indices de la tabla `especialidad`
+--
+ALTER TABLE `especialidad`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `frutas`
+--
+ALTER TABLE `frutas`
+  ADD PRIMARY KEY (`id_fruta`);
+
+--
+-- Indices de la tabla `mascotas`
+--
+ALTER TABLE `mascotas`
+  ADD PRIMARY KEY (`id_com`);
+
+--
+-- Indices de la tabla `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`documento`),
+  ADD UNIQUE KEY `slug` (`slug`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `documento` (`documento`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `agendamiento`
+--
+ALTER TABLE `agendamiento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `especialidad`
+--
+ALTER TABLE `especialidad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `frutas`
+--
+ALTER TABLE `frutas`
+  MODIFY `id_fruta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `mascotas`
+--
+ALTER TABLE `mascotas`
+  MODIFY `id_com` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `agendamiento`
+--
+ALTER TABLE `agendamiento`
+  ADD CONSTRAINT `agendamiento_ibfk_2` FOREIGN KEY (`documento`) REFERENCES `usuarios` (`documento`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `agendamiento_ibfk_3` FOREIGN KEY (`documento_doctor`) REFERENCES `doctor` (`doc_documento`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
